2a58c0616ae740823af1856c11581f1639e56c01
darmsDD <ivandinizdobbin2@gmail.com>
2021-10-21 10:41:22 -0300

    Altera os nomes dos defines para nomes mais claros

--- src/sensor.h
+++ src/sensor.h
@@ -10,20 +10,20 @@
 #include <softPwm.h>
 #include "motor.h"
 
-#define frontal1_trigger 4
-#define frontal1_echo 5
-#define frontal2_trigger  6
-#define frontal2_echo 10
+#define frontal_esquerda_trigger 4
+#define frontal_esquerda_echo 5
+#define frontal_direita_trigger  12
+#define frontal_direita_echo 13
 
-#define traseiro1_trigger 11
-#define traseiro1_echo 31
-#define traseiro2_trigger 4
-#define traseiro2_echo 5
+#define traseiro_esquerdo_trigger 11
+#define traseiro_esquerdo_echo 31
+#define traseiro_direito_trigger 4
+#define traseiro_direito_echo 5
 
-#define lateral1_trigger 0
-#define lateral1_echo  2
-#define lateral2_trigger 12
-#define lateral2_echo 13
+#define lateral_esquerda_trigger 0
+#define lateral_esquerda_echo  2
+#define lateral_direita_trigger 6
+#define lateral_direita_echo 10
 
 
 
